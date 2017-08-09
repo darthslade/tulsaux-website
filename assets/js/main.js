75d8c10ab24cd4053213d8b6177312a6b7747657
Justin Hooper <jhooper@broco.com>
2017-08-09 12:15:02 -0500

    went to plain js for date function and removed jQuery document ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
-$( document ).ready(function() {
-    /* Copyright Date Append
-    --------------------------------------------------------------------------- */
-    document.getElementsByClassName("copy-date").innerHTML = new Date().getFullYear();
+function run() {
+    document.getElementById("copy-date").innerHTML = new Date().getFullYear();
+}
 
+// in case the document is already rendered
+if (document.readyState!='loading') run();
+// modern browsers
+else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run);
+// IE <= 8
+else document.attachEvent('onreadystatechange', function(){
+    if (document.readyState=='complete') run();
 });
